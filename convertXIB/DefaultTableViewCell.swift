9fff5baecc9638c921509b96204f8ef6d8d1b995
markatlarge <russoatlarge@gmail.com>
2017-02-16 08:07:01 -0500

    Initial Commit

---
+++ convertXIB/DefaultTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  DefaultTableViewCell.swift
+//  convertXIB
+//
+//  Created by Mark Russo on 2/15/17.
+//  Copyright © 2017 markatlarge. All rights reserved.
+//
+
+import UIKit
+
+class DefaultTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ convertXIB/LandingViewController.swift
@@ -0,0 +1,41 @@
+//
+//  LandingViewController.swift
+//  convertXIB
+//
+//  Created by Mark Russo on 2/15/17.
+//  Copyright © 2017 markatlarge. All rights reserved.
+//
+
+import UIKit
+
+class LandingViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        addBottomSheetView()
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    func addBottomSheetView(scrollable: Bool? = true) {
+        let bottomSheetVC = ScrollableBottomSheetViewController()
+        
+        self.addChildViewController(bottomSheetVC)
+        self.view.addSubview(bottomSheetVC.view)
+        bottomSheetVC.didMove(toParentViewController: self)
+        
+        let height = view.frame.height
+        let width  = view.frame.width
+        bottomSheetVC.view.frame = CGRect(x: 0, y: self.view.frame.maxY, width: width, height: height)
+    }
+
+
+}
